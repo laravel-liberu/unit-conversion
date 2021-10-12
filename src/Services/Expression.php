07ffda1214156194a400e0aa28e4483dee137bb7
Mihai <mihai.ocneanu@gmail.com>
2021-10-12 12:59:34 +0300

    added Centimeter Length conversion; fixed Expression validation regexp

--- src/Services/Expression.php
+++ src/Services/Expression.php
@@ -8,7 +8,7 @@ class Expression
 {
     public static function validate(string $expression): void
     {
-        preg_match('/[+-]?([0-9]*[.])?[0-9]+ [a-z]+/', $expression, $matches);
+        preg_match('/[+-]?\d*\.?\d+ \w+/', $expression, $matches);
 
         if (count($matches) !== 1 || $matches[0] !== $expression) {
             throw Exception::invalid($expression);
